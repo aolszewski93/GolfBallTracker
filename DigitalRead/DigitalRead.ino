f40bcec8d5f67e9102f22171d0ad1ddc3046d871
Albert Olszewski <al.m.olszewski@gmail.com>
2022-02-11 21:23:55 -0500

    added 3 balls to digital read

--- DigitalRead/DigitalRead.ino
+++ DigitalRead/DigitalRead.ino
@@ -9,15 +9,30 @@
 */
 
 // digital pin 0 has a pushbutton attached to it. Give it a name:
-int IRSensor = 5;
-int IRState = 1; //sets pin state to off
+int IRSensor1 = 5;
+int IRSensor2 = 4;
+int IRSensor3 = 14;
+int IRState1 = 1; //sets pin state to off
+int IRState2 = 1; //sets pin state to off
+int IRState3 = 1;
+
+void balllife(int IR, int ballnumber) {
+  // conditional to tell whether IR is on or off
+  if (IR == 0){
+    Serial.println(String(ballnumber) +"Ball is Life"); // print out
+  }
+  else{
+    Serial.println(String(ballnumber) +"Ball is Lost"); // inform ball is lost
+  }
+}
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(115200);
   // make the pushbutton's pin an input:
-  pinMode(IRSensor, INPUT_PULLUP);
+  pinMode(IRSensor1, INPUT_PULLUP);
+  pinMode(IRSensor2, INPUT_PULLUP);
 }
 
 // the loop routine runs over and over again forever:
@@ -26,16 +41,13 @@ void loop() {
   // conditional that sends a message to serial whenver the sensor changes state
   //if (IRState != digitalRead(IRSensor)) {
     
-    IRState = digitalRead(IRSensor); //update the pin state
-    
-    // conditional to tell whether IR is on or off
-    if (IRState == 0){
-      Serial.println("Ball is Life"); // print out
-    }
-    else{
-      Serial.println("Ball is Lost"); // inform ball is lost
-    }
+    IRState1 = digitalRead(IRSensor1); //update the pin state
+    IRState2 = digitalRead(IRSensor2); //update the pin state
+    IRState3 = digitalRead(IRSensor3);
+    balllife(IRState1, IRSensor1);
+    balllife(IRState2, IRSensor2);
+    balllife(IRState3, IRSensor3);
     
   //}
-  delay(100);        // delay in between reads for stability
+  delay(1000);        // delay in between reads for stability
 }
